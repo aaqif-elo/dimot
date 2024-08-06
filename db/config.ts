7611ff92a469375254b9915290f42174ac9a309f
aaqif_elo <aaqif@elobyte.com>
2024-08-06 16:35:12 -0700

    ### Commit Message
    #### Subject:
    Add tables for polls, choices, and votes in database configuration
    
    #### Body:
    Introduced `Poll`, `PollChoice`, and `Vote` tables to the database configuration to support poll functionality. Each table includes relevant columns such as IDs, references to other tables, timestamps, and default values for certain fields. This enhancement aims to facilitate structured storage and management of poll data, improving the ability to handle user-generated polls and votes within the application. The relationships between tables ensure data integrity and enable more complex queries related to poll interactions.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,43 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from "astro:db";
 
-// https://astro.build/db/config
+const Poll = defineTable({
+  columns: {
+    id: column.number({
+      primaryKey: true,
+    }),
+    // A string of text.
+    author: column.text({}),
+    question: column.text({}),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW }),
+    voteCount: column.number({ default: 0 }),
+    verified: column.boolean({ default: false }),
+  },
+});
+
+const PollChoice = defineTable({
+  columns: {
+    id: column.number({
+      primaryKey: true,
+    }),
+    pollId: column.number({ references: () => Poll.columns.id }),
+    choice: column.text({}),
+    voteCount: column.number({ default: 0 }),
+  },
+});
+
+const Vote = defineTable({
+  columns: {
+    id: column.number({
+      primaryKey: true,
+    }),
+    pollChoiceId: column.number({ references: () => PollChoice.columns.id }),
+    createdAt: column.date({ default: NOW }),
+    username: column.text({ optional: true }),
+    ip: column.text({ optional: true }),
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Poll, PollChoice, Vote },
 });
